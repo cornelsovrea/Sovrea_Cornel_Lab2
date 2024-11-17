76f9a36d71801957ad1c24e22e9039e976b2f5c8
acer <acer@DESKTOP-0DLP7RR>
2024-11-17 22:36:32 +0200

    Add project files.

---
+++ Data/Sovrea_Cornel_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sovrea_Cornel_Lab2.Models;
+
+namespace Sovrea_Cornel_Lab2.Data
+{
+    public class Sovrea_Cornel_Lab2Context : DbContext
+    {
+        public Sovrea_Cornel_Lab2Context (DbContextOptions<Sovrea_Cornel_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sovrea_Cornel_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Sovrea_Cornel_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sovrea_Cornel_Lab2.Data;
+using Sovrea_Cornel_Lab2.Models;
+
+namespace Sovrea_Cornel_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Sovrea_Cornel_Lab2.Data.Sovrea_Cornel_Lab2Context _context;
+
+        public IndexModel(Sovrea_Cornel_Lab2.Data.Sovrea_Cornel_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher).ToListAsync();
+        }
+    }
+}
